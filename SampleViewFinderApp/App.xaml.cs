48a9e1d53eb0da595860192f966f46b9d155fe73
Keith Pickford <Keith@FunctionZero.com>
2023-04-12 16:48:02 +0100

    Added ViewFinderApp.

---
+++ SampleViewFinderApp/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace SampleViewFinderApp
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
