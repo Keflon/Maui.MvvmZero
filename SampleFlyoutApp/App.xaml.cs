00112b2978c3abc0af1c3a9e8c1a1f2d12c84d4b
Keith Pickford <Keith@FunctionZero.com>
2023-04-14 14:25:40 +0100

    Cleanup. Version bump.

--- SampleFlyoutApp/App.xaml.cs
+++ SampleFlyoutApp/App.xaml.cs
@@ -6,22 +6,6 @@ namespace SampleFlyoutApp
 {
     public partial class App : Application
     {
-        //public App(FlyoutPage mainPage, IPageServiceZero pageService)
-        //{
-        //    InitializeComponent();
-
-        //    pageService.Init(this);
-
-        //    var flyout = pageService.GetMvvmPage<FlyoutContentPage, FlyoutContentPageVm>().page;
-
-        //    //mainPage.Flyout = new NavigationPage(flyout) { Title = "Hello" };
-        //    mainPage.Flyout = flyout;
-        //    mainPage.Flyout.Navigation.PushAsync(pageService.GetMvvmPage<FlyoutContentPage, FlyoutContentPageVm>().page);
-        //    // Detail cannot be null. Will be overwritten immediately after being presented.
-        //    mainPage.Detail = new ContentPage();
-        //    MainPage = mainPage;
-        //}
-
         public App(IPageServiceZero pageService)
         {
             InitializeComponent();
