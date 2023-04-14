263de2ef626fa2330bd19a5331693b46cfd4dd45
Keith Pickford <Keith@FunctionZero.com>
2023-04-14 16:34:15 +0100

    Improved exception messaging.

---
+++ Maui.MvvmZero/Implementation/ViewMapperException.cs
@@ -0,0 +1,14 @@
+﻿using System.Diagnostics;
+using System.Runtime.Serialization;
+
+namespace FunctionZero.Maui.MvvmZero
+{
+    [Serializable]
+    internal class ViewMapperException : Exception
+    {
+        public ViewMapperException(string message, Exception innerException) : base(message, innerException)
+        {
+            Debug.WriteLine(message);
+        }
+    }
+}
