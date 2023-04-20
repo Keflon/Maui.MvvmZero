2a4cbaf318360ce83ab9d5e1a2d48894c33f0a21
Keith Pickford <Keith@FunctionZero.com>
2023-04-20 15:33:54 +0100

    Improved DefaultNavigationFinder. Introduced DefaultMultiPageFinder.

--- Maui.MvvmZero/Implementation/PageServiceAppBuilderExtensions.cs
+++ Maui.MvvmZero/Implementation/PageServiceAppBuilderExtensions.cs
@@ -37,30 +37,36 @@ namespace FunctionZero.Maui.MvvmZero
             return appBuilder;
         }
 
-        private static MultiPage<Page> DefaultMultiPageFinder()
-        {
-            throw new NotImplementedException();
-        }
-
+        #region NavigationFinder
         /// <summary>
         /// This ought to find the correct INavigation for most people.
         /// It can be replaced in the UsePageService delegate if necessary.
+        /// Method: Find the current Page instance. 
+        /// Method: If it is on a nav stack of a NavigationPage, return the INavigation.
+        /// Method: 
         /// </summary>
         private static INavigation DefaultNavigationFinder()
         {
-            return DefaultNavigationFinder(Application.Current.MainPage);
+            return DefaultNavigationFinder(Application.Current.MainPage, null);
         }
 
-        private static INavigation DefaultNavigationFinder(Page current)
+        private static INavigation DefaultNavigationFinder(Page current, INavigation lastNavigation)
         {
             if (current is FlyoutPage flyoutPage)
-                return DefaultNavigationFinder(flyoutPage.Detail);
+                // Reset lastNavigation and recurse.
+                return DefaultNavigationFinder(flyoutPage.Detail, null);
 
             if (current is MultiPage<Page> multiPage)
-                return DefaultNavigationFinder(multiPage.CurrentPage);
+                // Reset lastNavigation and recurse.
+                return DefaultNavigationFinder(multiPage.CurrentPage, null);
 
-            if (current is Page page)
-                return GetNavigationPageForPage(page);
+            if (current is NavigationPage navPage)
+                // navPage.Navigation is a candidate. Recurse with it.
+                return DefaultNavigationFinder(navPage.CurrentPage, navPage.Navigation);
+
+            if (current is Page)
+                // If the Page instance is on a nav-stack, lastNavigation will not be null.
+                return lastNavigation;
 
             return null;
         }
@@ -74,18 +80,55 @@ namespace FunctionZero.Maui.MvvmZero
         // TODO: then pushing a page from ContentPage1 will push it onto the TabbedPage1[selectedTab] stack, and won't be seen until 
         // TODO: we pop back to it.
         // TODO: I think it better to return null in this case. 
-        private static INavigation GetNavigationPageForPage(Page page)
-        {
-            while (page != null)
-            {
-                if (page is NavigationPage navigationPage)
-                    return navigationPage.Navigation;
-                else if (page is MultiPage<Page>)
-                    return null;
+        //private static INavigation GetNavigationPageForPage(Page page)
+        //{
+        //    while (page != null)
+        //    {
+        //        if (page is NavigationPage navigationPage)
+        //            return navigationPage.Navigation;
+        //        else if (page is MultiPage<Page>)
+        //            return null;
 
-                    page = page.Parent as Page;
-            }
+        //            page = page.Parent as Page;
+        //    }
+        //    return null;
+        //}
+        #endregion
+
+        #region MultiPageFinder
+
+        private static MultiPage<Page> DefaultMultiPageFinder()
+        {
+            return DefaultMultiPageFinder(Application.Current.MainPage, null);
+
+        }
+
+        private static MultiPage<Page> DefaultMultiPageFinder(Page current, MultiPage<Page> lastMultiPage)
+        {
+            if (current is FlyoutPage flyoutPage)
+                // Reset lastMultiPage and recurse.
+                return DefaultMultiPageFinder(flyoutPage.Detail, null);
+
+            if (current is MultiPage<Page> multiPage)
+                // multiPage is a candidate. Recurse with it.
+                return DefaultMultiPageFinder(multiPage.CurrentPage, multiPage);
+
+            if (current is NavigationPage navPage)
+                // if navPage is multiPage.Current, lastMultiPage is a viable candidate.
+                if (navPage == lastMultiPage?.CurrentPage)
+                    return DefaultMultiPageFinder(navPage.CurrentPage, lastMultiPage);
+                else
+                    // SMELL: Try this out and see what happens.
+                    // For if a NavigationPage is pushed onto a NavigationPage. (Should never happen?)
+                    return DefaultMultiPageFinder(navPage.CurrentPage, null);
+
+            if (current is Page page)
+                // lastMultiPage will be non-null only if Page is visible, and belongs to a MultiPage.
+                return lastMultiPage;
+
             return null;
         }
+
+        #endregion
     }
 }
