5e3982e9e19a166fb656a71d1dc980c724e5498e
Keith Pickford <Keith@FunctionZero.com>
2023-04-20 22:50:35 +0100

    Ice cold bugfix in ViewModelDataTemplateSelector. :)

--- Maui.MvvmZero/Implementation/ViewModelDataTemplateSelector.cs
+++ Maui.MvvmZero/Implementation/ViewModelDataTemplateSelector.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -25,17 +26,37 @@ namespace FunctionZero.Maui.MvvmZero
         private object GetTemplate(object item)
         {
             // 'item' is  our view-model. Get the corresponding Page.
-            var page = (IView)_templateGetter(item.GetType());
+            var page = (Page)_templateGetter(item.GetType());
             // initialize it. If the initializer returns true, wrap it in a NavigationPage.
             if (_initializer(item) == true)
             {
+                // Set the page BC ...
+                page.BindingContext = item;
                 var root = _navPageGetter();
+                // And remove the Nav BC as soon as it is set.
+                root.BindingContextChanged += Root_BindingContextChanged;
                 root.Title = ((Page)page).Title;
                 root.PushAsync((Page)page, false);
                 return root;
             }
             else
                 return page;
+        }
+
+        /// <summary>
+        /// The caller to OnSelectTemplate will set the BindingContext of the object returned by GetTemplate.
+        /// Normally this is fine, but if we've wrapped the return value in a NavigationPage, we do not want to set the 
+        /// BindingContext of the NavigationPage, otherwise we'll e.g. get OwnerPageAppearing events raised twice for 
+        /// the viewmodel associated with the NavigationPage root Page.
+        /// To counter this, we remove the BindingContext as soon as it has been set.
+        /// </summary>
+        private void Root_BindingContextChanged(object sender, EventArgs e)
+        {
+            var nav = (NavigationPage)sender;
+            // Prevent unintended side effects ...
+            nav.BindingContextChanged -= Root_BindingContextChanged;
+
+            nav.ClearValue(NavigationPage.BindingContextProperty);
         }
     }
 }
